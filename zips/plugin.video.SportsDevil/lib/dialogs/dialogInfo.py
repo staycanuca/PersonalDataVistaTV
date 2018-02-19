cfb6afc997e5d2ff53f0ad5d9c359126aa618d62
biglad <biglad@mgawow.co.uk>
2018-02-19 19:19:41 +0000

    ..
    ..

--- zips/plugin.video.SportsDevil/lib/dialogs/dialogInfo.py
+++ zips/plugin.video.SportsDevil/lib/dialogs/dialogInfo.py
@@ -6,12 +6,12 @@ class DialogInfo:
 
     def __init__(self):
         self.dlg = xbmcgui.Dialog()
-        self.head = 'Cerebro IPTV (Sports Devil)'
+        self.head = 'Vista IPTV (Sports Devil)'
 
     def show(self, message):
 
         #self.dlg.close()
         dp = xbmcgui.DialogProgress()
-        dp.create("[COLOR=gold][B]Cerebro IPTV Hunter[/COLOR][/B]","This can take 2-45 seconds.(based on host & your device/speed)","It may look like its frozen its not, it's opening the stream... ","Please Wait!!!!")
+        dp.create("[COLOR=gold][B]Vista IPTV[/COLOR][/B]","This can take 2-45 seconds.(based on host & your device/speed)","It may look like its frozen its not, it's opening the stream... ","Please Wait!!!!")
         if dp: dp.close()
         self.dlg.ok(self.head, "Stream Down")
