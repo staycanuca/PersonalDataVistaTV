360fe9573d2a0dc2c4089b9bae4e3911f225f380
biglad <biglad@mgawow.co.uk>
2018-02-20 19:23:28 +0000

    ..
    ..

--- zips/script.vista.ondemanmenu/addon.py
+++ zips/script.vista.ondemanmenu/addon.py
@@ -12,12 +12,14 @@ def menuoptions():
         function1,
         function2,
 		function3,
-		function4
+		function4,
+		function5
         )
         
     call = dialog.select('[B][COLOR=yellow]On Demand Menu[/COLOR][/B]', [ 
     '[B]      Movies[/B]' ,	
     '[B]      TV Shows[/B]',
+	'[B]      Latest TV Episodes[/B]',
 	'[B]      Vidics[/B]',
 	'[B]      Sky Cinema on Demand[/B]',
     ])
@@ -29,7 +31,7 @@ def menuoptions():
         # esc is not pressed
         if call < 0:
             return
-        func = funcs[call-4]
+        func = funcs[call-5]
         return func()
     else:
         func = funcs[call]
@@ -46,9 +48,12 @@ def function2():
     xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.vistatv/?action=tvNavigator",return)')
 	
 def function3():
-    xbmc.executebuiltin('RunAddon(plugin.video.vistavidics)')
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.tvheaven/?description&iconimage=C%3a%5cUsers%5cbigla%5cAppData%5cRoaming%5cKodi%5caddons%5cplugin.video.tvheaven%5cresources%5cart%5clatest.png&mode=1&name=New%20Latest%20Episodes&url=http%3a%2f%2fwatchepisodeseries.unblockall.org%2f",return)')
 	
 def function4():
+    xbmc.executebuiltin('RunAddon(plugin.video.vistavidics)')
+	
+def function5():
     xbmc.executebuiltin('ActivateWindow(10025,"plugin://plugin.video.vistatv/?action=channels",return)')
 		  
 menuoptions()
