223fd5695b2b92b68bdaf232f55d0d49f2deb61a
biglad <biglad@mgawow.co.uk>
2018-02-20 15:32:05 +0000

    ..
    ..

---
+++ zips/script.vista.ondemanmenu/addon.py
@@ -0,0 +1,44 @@
+import time
+import xbmc
+import os
+import xbmcgui
+import urllib2
+import utils
+import sfile
+
+def menuoptions():
+    dialog = xbmcgui.Dialog()
+    funcs = (
+        function1,
+        function2
+        )
+        
+    call = dialog.select('[B][COLOR=yellow]Live TV Menu[/COLOR][/B]', [ 
+    '[B]      >> [COLOR=gold]Open[/COLOR] [COLOR=gold]TV Guide[/COLOR] << [/B]' ,	
+    '[B]      >> [COLOR=yellow]Open IPTV Lists[/COLOR] << Live TV (Many Options)[/B]',
+    ])
+    # dialog.selectreturns
+    #   0 -> escape pressed
+    #   1 -> first item
+    #   2 -> second item
+    if call:
+        # esc is not pressed
+        if call < 0:
+            return
+        func = funcs[call-2]
+        return func()
+    else:
+        func = funcs[call]
+        return func()
+    return 
+
+
+
+    
+def function1():
+    xbmc.executebuiltin('RunAddon(script.tvguide.vistatv)')
+	
+def function2():
+    xbmc.executebuiltin('ActivateWindow(10025,"plugin://script.module.vistatvlive/",return)')
+		  
+menuoptions()
