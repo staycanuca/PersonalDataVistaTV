36c678638b7d9276d91785c1171d1ca632129873
biglad <biglad@mgawow.co.uk>
2018-02-23 14:19:42 +0000

    ..
    ..

--- zips/script.cachesystem/startup.py
+++ zips/script.cachesystem/startup.py
@@ -6,7 +6,7 @@ import urllib2
  
 
 #xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(script.cachesystem),5,silent)")
-xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(script.cachesystem),1)")
+xbmc.executebuiltin("XBMC.AlarmClock('MTVBCS',XBMC.RunAddon(script.cachesystem),1,silent)")
 
 
 	
